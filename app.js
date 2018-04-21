9d2c930de17f08425659f0fb4771f3d4c60fb747
Shirin <shirinjadib@gmail.com>
2018-04-21 01:03:46 -0700

    udemy practice

---
+++ app.js
@@ -0,0 +1,12 @@
+var answer = prompt("are we there yet?");
+
+while(answer !== "yes") {
+  var answer = prompt("are we there yet?");
+}
+
+
+if(answer === "yes") {
+  alert("Yay, We made it!")
+} else {
+  var answer = prompt("are we there yet?");
+}
